0a06d6812e3069e913e9ae709f74c360dee10b30
Alan <alansal_11@hotmail.com>
2020-05-15 00:23:20 -0500

    Created the base.js file

---
+++ src/js/views/base.js
@@ -0,0 +1,28 @@
+export const elements = {
+    search: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
+    searchResultList: document.querySelector('.results__list')
+}
+
+export const elementString = {
+    loader: 'loader'
+}; 
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementString.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementString.loader}`);
+    if (loader) {
+        loader.parentElement.removeChild(loader);
+    }
+}
