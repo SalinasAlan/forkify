1c9e1d60a15e442e14ab2e084591fe5e9cab75bf
Alan <alansal_11@hotmail.com>
2020-05-21 00:51:10 -0500

    The view of the list was corrected

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -2,7 +2,7 @@ import { elements } from './base';
 
 export const renderItem = item => {
     const markup = `
-    <li class="shopping__item" data-itemId=${item.id}>
+    <li class="shopping__item" data-itemid="${item.id}">
         <div class="shopping__count">
             <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
             <p>${item.unit}</p>
@@ -22,5 +22,7 @@ export const renderItem = item => {
 
 export const deleteItem = id => {
     const item = document.querySelector(`[data-itemId="${id}"]`);
-    item.parentElement.removeChild(item);
+    if (item) {
+        item.parentElement.removeChild(item);
+    }
 };
